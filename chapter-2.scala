c1f808fb71249457a63514b3f091501e8624fb8d
Joe San <codeintheopen@gmail.com>
2017-04-11 22:21:00 +0200

    Update chapter-2.scala

--- chapter-2.scala
+++ chapter-2.scala
@@ -1,4 +1,4 @@
-// Exercise 2.2
+ // Exercise 2.2
  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
 
     @tailrec
@@ -16,7 +16,21 @@
     check(as)
   }
 
-  println(isSorted(Array(1, 3, 2), (x: Int, y: Int) => x < y)) // prints false
-  println(isSorted(Array(1, 2, 3), (x: Int, y: Int) => x < y)) // prints true
-  println(isSorted(Array(1), (x: Int, y: Int) => x < y)) // prints true
-  println(isSorted(Array(1, 2), (x: Int, y: Int) => x < y)) // prints true
+  // some tests for our isSorted (all of them pass)
+  println(isSorted(Array(1, 3, 2), (x: Int, y: Int) => x < y))   // prints false
+  println(isSorted(Array(1, 2, 3), (x: Int, y: Int) => x < y))   // prints true
+  println(isSorted(Array(1), (x: Int, y: Int) => x < y))         // prints true
+  println(isSorted(Array(1, 2), (x: Int, y: Int) => x < y))      // prints true
+  println(isSorted(Array.empty[Int], (x: Int, y: Int) => x < y)) // prints true
+
+ // Exercise 2.3
+ def curry[A, B, C](f: (A, B) => C): A => (B => C) = 
+   (a: A) => (b: B) => f(a, b)
+
+ // Exercise 2.4 - Note f: A => B => C is also written as f: A => (B => C) (right associativity)
+ def uncurry[A, B, C](f: A => B => C): (A, B) => C = 
+   (a: A, b: B) => f(a)(b)
+
+ // Exercise 2.5
+ def compose[A, B, C](f: B => C, g: A => B): A => C =
+   (a: A) => f(g(a))
