6355013cef1294735bb901dae51148c36a27ebe3
Joe San <codeintheopen@gmail.com>
2017-04-12 22:53:31 +0200

    Update chapter-4.scala

--- chapter-4.scala
+++ chapter-4.scala
@@ -19,3 +19,9 @@
     val mean = mean(xs)
     mean.flatMap(elem => mean(elem.map(x => math.pow(x - mean, 2))))
   }
+
+  // Exercise 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (None, _) | (_, None) => None
+    case (Some(a), Some(b)) => Some(f(a, b))
+  }
