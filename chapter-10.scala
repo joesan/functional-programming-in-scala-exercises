f0dcd007d0cb32e2661adf23051d3451ebb93ccc
Joe San <codeintheopen@gmail.com>
2017-04-14 16:40:17 +0200

    Update chapter-10.scala

--- chapter-10.scala
+++ chapter-10.scala
@@ -1,5 +1,20 @@
   // Exercise 10.1
   val intAddition: Monoid[Int] = new Monoid[Int] {
-    def op(a1: Int, a2: Int) = a1 + a2
+    def op(a: Int, b: Int) = a + b
     def zero = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op(a: Int, b: Int) = a * b
+    def zero = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a: Boolean, b: Boolean) = a || b
+    def zero = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a: Boolean, b: Boolean) = a && b
+    val zero = true
   }
