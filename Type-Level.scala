22f65be1ae7ffe2173978135eccce4c7aa4e3398
Joe San <codeintheopen@gmail.com>
2017-05-12 10:28:08 +0200

    Update Type-Level.scala

--- Type-Level.scala
+++ Type-Level.scala
@@ -5,10 +5,39 @@
  */
  
  // Let's first draw an implementation of Peano numbers at the value level:
- sealed trait PeanoNum { def + (that: Nat): Nat }
+ sealed trait PeanoNum { def + (that: PeanoNum): PeanoNum }
+
  case object Zero extends PeanoNum {
-   def + (that: Nat): Nat = that
+   def + (that: PeanoNum): PeanoNum = that
  }
- case object Next(n: Nat) extends PeanoNum {
-   def + (that: Nat): Nat = Nat(n + that) 
+
+ case class Next(n: PeanoNum) extends PeanoNum {
+   def + (that: PeanoNum): PeanoNum = Next(n + that) 
  }
+
+ // Now some test cases to see how our value types hold up!
+scala> val zero = Zero
+zero: Zero.type = Zero
+
+scala> val one = Next(Zero)
+one: Next = Next(Zero)
+
+scala> val two = Next(one)
+two: Next = Next(Next(Zero))
+
+scala> val three = Next(two)
+three: Next = Next(Next(Next(Zero)))
+
+scala> val four = Next(three)
+four: Next = Next(Next(Next(Next(Zero))))
+
+scala> one + two == three
+res0: Boolean = true
+
+scala> two + three == four
+res1: Boolean = false
+
+scala> three + one == four
+res2: Boolean = true
+
+// so far so good!! Now let's translate this into types instead of values!!
